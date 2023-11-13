42cff1cf6ccc32bdd6b9eb55c784775a4dad9dff
Isaque <isaque-cpaulo@hotmail.com>
2023-11-13 17:30:39 -0300

    Menu lateral

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel ="stylesheet" href = "asset/css/style.css">
+    
+    <title>Guardião</title>
+</head>
+<body>
+  <nav class ="menu-lateral">
+    <div class = "btn-expandir">
+      <i class="bi bi-list" id="btn-exp"></i>
+    </div>
+    <ul>
+      <li class ="item-menu ativo">
+          <a href="#">
+            <span class = "icon"> <i class="bi bi-house-door"></i>
+        </span>
+            <span class = "txt-link"> Home </span>
+          </a>
+      </li>
+      <li class ="item-menu">
+          <a href="#">
+            <span class = "icon"> <i class="bi bi-person-arms-up"></i>
+            </span>
+            <span class = "txt-link"> Heros </span>
+          </a>
+      </li>
+      <li class ="item-menu">
+          <a href="#">
+            <span class = "icon"><i class="bi bi-yin-yang"></i>
+            </span>
+            <span class = "txt-link"> S.O.S</span>
+          </a>
+      </li>
+
+    </ul>
+  </nav>
+   <script src = "asset/js/menu.js"></script>
+
+</body>
+</html>
